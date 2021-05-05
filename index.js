a8c05bbb3e5d303524630c4416d23924c0d04618
kalille Overton <>
2021-05-05 18:16:23 -0400

    commit tree 1dcf685c892b7f78f73054a5669908e759e09747

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(array,string){
+    let match = array.filter(name=> string.toUpperCase() === name.toUpperCase());{
+        
+        return match// return match.toUpperCase()
+    }
+}
+function fuzzyMatch(array,string){
+let fuzzy = array.filter(name=> name.substring(0,string.length)=== string);
+    return fuzzy
+}
+function matchName(array,string){
+    let gotIt = array.filter(obj=> obj.name=== string);
+    return gotIt
+
+}
+
